402f5ded92f0646bd15fbf00eb799a719c65676d
biels25 <ga.eduardo.correa@gmail.com>
2025-04-09 19:54:49 -0300

    projetos

---
+++ ProjetoInterfaceMultifuncoes/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ProjetoInterfaceMultifuncoes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.lblPotenciometro = new System.Windows.Forms.Label();
+            this.lblSensorTemp = new System.Windows.Forms.Label();
+            this.timerCOM = new System.Windows.Forms.Timer(this.components);
+            this.picBoxLED = new System.Windows.Forms.PictureBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLED)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(2, -1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(799, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Controls.Add(this.btnConectar);
+            this.tabPage1.Controls.Add(this.lblPotenciometro);
+            this.tabPage1.Controls.Add(this.lblSensorTemp);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(791, 424);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.picBoxLED);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(791, 424);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(309, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(199, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(17, 6);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(175, 57);
+            this.btnConectar.TabIndex = 8;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // lblPotenciometro
+            // 
+            this.lblPotenciometro.AutoSize = true;
+            this.lblPotenciometro.Location = new System.Drawing.Point(688, 389);
+            this.lblPotenciometro.Name = "lblPotenciometro";
+            this.lblPotenciometro.Size = new System.Drawing.Size(75, 13);
+            this.lblPotenciometro.TabIndex = 6;
+            this.lblPotenciometro.Text = "Potenciometro";
+            // 
+            // lblSensorTemp
+            // 
+            this.lblSensorTemp.AutoSize = true;
+            this.lblSensorTemp.Location = new System.Drawing.Point(34, 389);
+            this.lblSensorTemp.Name = "lblSensorTemp";
+            this.lblSensorTemp.Size = new System.Drawing.Size(100, 13);
+            this.lblSensorTemp.TabIndex = 5;
+            this.lblSensorTemp.Text = "SensorTemepratura";
+            // 
+            // timerCOM
+            // 
+            this.timerCOM.Enabled = true;
+            this.timerCOM.Interval = 1000;
+            this.timerCOM.Tick += new System.EventHandler(this.timerCOM_Tick);
+            // 
+            // picBoxLED
+            // 
+            this.picBoxLED.Location = new System.Drawing.Point(245, 56);
+            this.picBoxLED.Name = "picBoxLED";
+            this.picBoxLED.Size = new System.Drawing.Size(179, 166);
+            this.picBoxLED.TabIndex = 8;
+            this.picBoxLED.TabStop = false;
+            this.picBoxLED.Click += new System.EventHandler(this.picBoxLED_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxLED)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Label lblPotenciometro;
+        private System.Windows.Forms.Label lblSensorTemp;
+        private System.Windows.Forms.Timer timerCOM;
+        private System.Windows.Forms.PictureBox picBoxLED;
+    }
+}
+
---
+++ ProjetoInterfaceMultifuncoes/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoInterfaceMultifuncoes
+{
+    public partial class Form1 : Form
+    {
+        private StringBuilder bufferRecebido = new StringBuilder();
+
+        public Form1()
+        {
+            InitializeComponent();
+            timerCOM.Interval = 2000;
+            timerCOM.Start();
+            picBoxLED.Tag = "Desligado";
+            picBoxLED.Image = Image.FromFile("");
+            picBoxLED.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                bufferRecebido.Append(serialPort1.ReadExisting());
+
+                if (bufferRecebido.ToString().Contains("\n"))
+                {
+                    this.Invoke((MethodInvoker)trataDadosRecebidos);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao receber dados: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void atualizaListaCOMs()
+        {
+            comboBox1.Items.Clear();
+            string[] portas = SerialPort.GetPortNames();
+
+            if (portas.Length == 0)
+            {
+                comboBox1.Items.Add("Nenhuma porta encontrada");
+                comboBox1.SelectedIndex = 0;
+                return;
+            }
+
+            foreach (string porta in portas)
+                comboBox1.Items.Add(porta);
+        }
+
+        private void trataDadosRecebidos()
+        {
+            try
+            {
+                string[] linhas = bufferRecebido.ToString().Split('\n');
+                bufferRecebido.Clear();
+
+                if (linhas.Length > 0)
+                {
+                    bufferRecebido.Append(linhas[linhas.Length - 1]);
+                }
+
+                // Agora dividimos os dados usando a vírgula como delimitador
+                string dadosRecebidos = linhas[0].Trim();
+                string[] dados = dadosRecebidos.Split(',');
+
+                if (dados.Length == 3)
+                {
+                    double temperatura = double.Parse(dados[0]);
+                    double tensaoA0 = double.Parse(dados[2]);
+
+                    // Atualiza os rótulos na interface
+                    lblSensorTemp.Text = temperatura.ToString("0.00") + " °C";
+                    lblPotenciometro.Text = tensaoA0.ToString("0.00") + " V";
+                }
+                else
+                {
+                    MessageBox.Show("Dados recebidos estão com formato incorreto.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao processar dados: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void conectarSerial()
+        {
+            if (comboBox1.SelectedItem == null || comboBox1.SelectedItem.ToString() == "Nenhuma porta encontrada")
+            {
+                MessageBox.Show("Selecione uma porta COM válida", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                serialPort1.PortName = comboBox1.SelectedItem.ToString();
+                serialPort1.BaudRate = 9600;
+                serialPort1.Parity = Parity.None;
+                serialPort1.StopBits = StopBits.One;
+                serialPort1.Handshake = Handshake.None;
+
+                serialPort1.Open();
+                btnConectar.Text = "Desconectar";
+                btnConectar.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao conectar: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void desconectarSerial()
+        {
+            try
+            {
+                serialPort1.Close();
+                comboBox1.Enabled = true;
+                btnConectar.Text = "Conectar";
+                btnConectar.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao desconectar: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                desconectarSerial();
+            }
+            else
+            {
+                conectarSerial();
+            }
+        }
+
+        private void timerCOM_Tick(object sender, EventArgs e)
+        {
+            atualizaListaCOMs();
+        }
+
+        private void picBoxLED_Click_1(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                try
+                {
+                    if (picBoxLED.Tag.ToString() == "Desligado")
+                    {
+                        serialPort1.Write("D\n");
+                        picBoxLED.Tag = "Ligado";
+                        picBoxLED.Image = Image.FromFile("");
+                        picBoxLED.SizeMode = PictureBoxSizeMode.StretchImage;
+                    }
+                    else
+                    {
+                        serialPort1.Write("L\n");
+                        picBoxLED.Tag = "Desligado";
+                        picBoxLED.Image = Image.FromFile("");
+                        picBoxLED.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao enviar comando: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
